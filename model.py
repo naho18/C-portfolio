ef0052dae25f65e8b0493438617dea5e17be240e
vagrant <vagrant@vagrant.vm>
2018-12-05 04:34:28 +0000

    added example data

--- model.py
+++ model.py
@@ -96,15 +96,52 @@ def example_data():
     inv3 = Investment(date_of_entry='2018-11-25', date_of_investment='2018-11-25',
         company_name='wonderFull', quantity='50000', cost='150000')
 
-    db.session.add_all([inv1, inv2, inv3])
+    inv4 = Investment(date_of_entry='2018-12-01', date_of_investment='2018-12-01',
+        company_name='goWarriors', quantity='120000', cost='120000')
+    inv5 = Investment(date_of_entry='2018-12-01', date_of_investment='2018-12-01',
+        company_name='capTable', quantity='100000', cost='200000')
+    inv6 = Investment(date_of_entry='2018-12-01', date_of_investment='2018-12-01',
+        company_name='Simple', quantity='70000', cost='150000')
+    inv7 = Investment(date_of_entry='2018-12-01', date_of_investment='2018-12-01',
+        company_name='chowNow', quantity='8800', cost='100000')
+    inv8 = Investment(date_of_entry='2018-12-01', date_of_investment='2018-12-01',
+        company_name='Honey', quantity='50000', cost='100000')
+    inv9 = Investment(date_of_entry='2018-12-01', date_of_investment='2018-12-01',
+        company_name='pineApple', quantity='111111', cost='100000')
+    inv10 = Investment(date_of_entry='2018-12-02', date_of_investment='2018-12-02',
+        company_name='SecurityNow', quantity='60000', cost='130000')
+    inv11 = Investment(date_of_entry='2018-12-02', date_of_investment='2018-12-02',
+        company_name='BlueGlass', quantity='77000', cost='150000')
+    inv12 = Investment(date_of_entry='2018-12-02', date_of_investment='2018-12-02',
+        company_name='HonestLeaves', quantity='45000', cost='145000')
+    inv13 = Investment(date_of_entry='2018-12-02', date_of_investment='2018-12-02',
+        company_name='LegalSolutions', quantity='100000', cost='170000')
+    inv14 = Investment(date_of_entry='2018-12-02', date_of_investment='2018-12-02',
+        company_name='Care4All', quantity='60000', cost='200000')
+    inv15 = Investment(date_of_entry='2018-12-02', date_of_investment='2018-12-02',
+        company_name='Smile', quantity='20000', cost='150000')
+    db.session.add_all([inv1, inv2, inv3, inv4, inv5, inv6, inv7, inv8, inv9,
+                        inv10, inv11, inv12, inv13, inv14, inv15])
     db.session.commit()
 
     # USERINV
     uinv1 = UserInv(user_id='1', inv_id='1')
     uinv2 = UserInv(user_id='1', inv_id='2')
     uinv3 = UserInv(user_id='2', inv_id='3')
-
-    db.session.add_all([uinv1, uinv2, uinv3])
+    uinv4 = UserInv(user_id='1', inv_id='4')
+    uinv5 = UserInv(user_id='1', inv_id='5')
+    uinv6 = UserInv(user_id='1', inv_id='6')
+    uinv7 = UserInv(user_id='1', inv_id='7')
+    uinv8 = UserInv(user_id='1', inv_id='8')
+    uinv9 = UserInv(user_id='1', inv_id='9')
+    uinv10 = UserInv(user_id='1', inv_id='10')
+    uinv11 = UserInv(user_id='1', inv_id='11')
+    uinv12 = UserInv(user_id='1', inv_id='12')
+    uinv13 = UserInv(user_id='1', inv_id='13')
+    uinv14 = UserInv(user_id='1', inv_id='14')
+    uinv15 = UserInv(user_id='1', inv_id='15')
+    db.session.add_all([uinv1, uinv2, uinv3, uinv4, uinv5, uinv6, uinv7, uinv8,
+                        uinv9, uinv10, uinv11, uinv12, uinv13, uinv14, uinv15])
     db.session.commit()
 
 if __name__ == "__main__":
@@ -116,3 +153,5 @@ if __name__ == "__main__":
     print "Connected to DB."
 
 # db.create_all()
+# example_data()
+
